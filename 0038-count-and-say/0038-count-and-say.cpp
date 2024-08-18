a9f4bc9c584196a3932aaab32288eeb431a1e683
Priyanshu Sarkar <164724656+Priyanshu-Sarkar03@users.noreply.github.com>
2024-08-18 19:41:21 +0530

    Time: 3 ms (78.64%), Space: 10.7 MB (26.18%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        // Base case for recursion
         if (n == 1)
             return "1";
+        
+        // Get the previous sequence by recursion
         string s = countAndSay(n - 1);
         string ans = "";
+        
+        // Iterate through the string s to construct the next sequence
         for (int i = 0; i < s.size(); i++) {
             char ch = s[i];
             int cnt = 1;
-            while (i < s.size()-1 && s[i] == s[i + 1]) {
+            
+            // Count how many times the current character repeats consecutively
+            while (i + 1 < s.size() && s[i] == s[i + 1]) {
                 cnt++;
                 i++;
             }
-          ans+=to_string(cnt);
-          ans+=string(1,ch);
+            
+            // Append the count and the character to the result string
+            ans += to_string(cnt);
+            ans += ch;
         }
+        
         return ans;
     }
 };
